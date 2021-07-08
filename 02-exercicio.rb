36a8fc466b184a34144f17426245fd9bbbf251c0
Higor Silva de Oliveira <h.igo.67@hotmail.com>
2021-07-08 17:44:08 -0300

    variaveis

--- 02-exercicio.rb
+++ 02-exercicio.rb
@@ -9,11 +9,15 @@ _1minuto = 60
 
 puts "Quantas horas há em um ano?"
 puts _1ano * _1dia
+
 puts "Quantos minutos há em uma década?"
 puts (10 * _1ano) * _1dia * _1hora
+
 puts "Qual sua idade em segundos?"
 puts (27 * _1ano) * _1dia * _1hora * _1minuto
+
 puts "Quantos chocolates você pretende comer na vida?"
 puts (80-27) * (3.0 / _1mes) * (12 * _1mes)
+
 puts "Se minha idade é 1232 milhões de segundos, qual é minha idade em anos?"
 puts 1232000000 / _1ano / _1dia / _1hora / _1minuto
