e95bb5f505cdcbe8f46c8a32578cfcf487c6c874
Higor Silva de Oliveira <h.igo.67@hotmail.com>
2021-07-08 15:24:06 -0300

    Primeiro Exercicio

---
+++ 01-exercicio.rb
@@ -0,0 +1,10 @@
+puts "Quantas horas há em um ano?"
+puts 365 * 24
+puts "Quantos minutos há em uma década?"
+puts (10 * 365) * 24 * 60
+puts "Qual sua idade em segundos?"
+puts (27 * 365) * 24 * 60 * 60
+puts "Quantos chocolates você pretende comer na vida?"
+puts (80-27) * 3 * 12
+puts "Se minha idade é 1232 milhões de segundos, qual é minha idade em anos?"
+puts 1232000000 / 365 / 24 / 60 / 60
