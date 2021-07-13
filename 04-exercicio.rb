21a1edd2e38f7142f1f8ba2db230c044dd999c1b
Higor Silva de Oliveira <h.igo.67@hotmail.com>
2021-07-13 09:46:45 -0300

    04-exercicio

--- 04-exercicio.rb
+++ 04-exercicio.rb
@@ -1,29 +1,29 @@
 # 1) O diâmetro de um círculo é 2x o seu raio.
 
-def diametro_do_circulo(x)
-    diametro = 2 * x
-    return diametro
+def diametro_do_circulo(raio)
+    return 2 * raio
+    
  end
-  
- diametro = diametro_do_circulo(20)
- puts diametro
+raio = 30
+puts diametro_do_circulo(raio)
 
  
  # 2) O comprimento de um círculo é seu diâmetro vezes o valor da constante matemática PI (3.1415...)
 
-  def comprimento_de_um_circulo(diametro)
-    comprimento = diametro * Math::PI
-    return comprimento
-end
-comprimento = comprimento_de_um_circulo(diametro)
-puts comprimento
+  def comprimento_de_um_circulo(raio)
+      return diametro_do_circulo(raio) * Math::PI
+    end
+
+    raio = 30
+    puts comprimento_de_um_circulo(raio)
+
 
  
 # 3) A área de um círculo é seu raio ao quadrado vezes o valor da constante matemática PI (3.1415...)
 
-def area_do_circulo(diametro)
-    area = ((diametro / 2) ** 2 ) * Math::PI
-    return area
+def area_do_circulo(raio)
+    return raio ** 2 * Math::PI
 end
-area = area_do_circulo(diametro)
-puts area
+
+raio = 30
+puts area_do_circulo(raio)
