f3806928061d6706382e3c6fa61ee6e2a446e430
Higor Silva de Oliveira <h.igo.67@hotmail.com>
2021-07-18 23:49:48 -0300

    10-exercicio

---
+++ 10-exercicio-parte-1.rb
@@ -0,0 +1,22 @@
+# 1) Desenhe um quadrado de 10 linhas por 10 colunas. Desenhar as bordas com “*”, preencher com “x”.
+# Enviar 10-exercicio-parte-1.rb
+
+for n in (1..10)
+    print "*"
+end
+puts
+for y in (1..8)
+    print "*"   
+   
+    for x in (1..8)
+        print "x"
+    end
+
+    puts "*"
+end
+
+for n in (1..10)
+    print "*"
+end
+
+    
---
+++ 10-exercicio-parte-2.rb
@@ -0,0 +1,10 @@
+x = "*"
+for n in (1..10)
+piramide = x * n
+puts piramide.to_s
+end
+
+for n in (1..10)
+    puts "*" * n
+end
+
